9ec174d6a8eac643cf33ffdc20ff1e0b4daea8eb
jrwells <jrwells10@gmail.com>
2016-02-24 12:38:13 -0500

    finish createGetByIdRoute tests

--- test/get.spec.js
+++ test/get.spec.js
@@ -5,9 +5,12 @@ import chaiAsPromised from 'chai-as-promised';
 import { cache } from './data/cache.js';
 import { TestServer, testModel, batchTestModel } from './utils/test-server.js';
 import { generateIndex,
+				 generateKeys,
 				 generateRange,
-				 generateRangeErrors,
- 				 arrayToRangeOutput } from './utils/helpers.js';
+				 generateRangeError,
+				 generateIdError,
+ 				 arrayToRangeOutput,
+			   objectToIdOutput			} from './utils/helpers.js';
 
 import { createGetLengthRoute,
 				 createGetRangesRoute,
@@ -52,7 +55,11 @@ const TYPE_ERROR_ID_UNDEFINED = 'TypeError: Cannot set property \'id\' of undefi
 const ID_REPEAT = 10;
 const ACCEPTED_KEYS = ['name', 'population', 'borough'];
 const ID_PATH = `${BASE_PATH}ById[{keys:ids}]${JSON.stringify(ACCEPTED_KEYS)}`;
-const idPromise = async (id) => cache.neighborhoodsById[id];
+const idPromise = async function (id) {
+	let o = cache.neighborhoodsById[id];
+	o.id = id;
+	return o;
+}
 
 describe("get core", function() {
 	describe('createGetLengthRoute', function() {
@@ -102,6 +109,7 @@ describe("get core", function() {
 											 createGetRangesRoute(BASE_PATH, rangePromise) ];
 				testServer.restart(routes);
 
+				// Generate expected output
 				let expectedOutput = arrayToRangeOutput(BASE_PATH, `${BASE_PATH}ById`,
 					await rangePromise(range.min, range.max), range.min);
 
@@ -119,6 +127,7 @@ describe("get core", function() {
 										 createGetRangesRoute(BASE_PATH, rangePromise) ];
 			testServer.restart(routes);
 
+			// Generate expected outputs
 			let expectedOutputOne = arrayToRangeOutput(BASE_PATH, `${BASE_PATH}ById`,
 				await rangePromise(rangeOne.min, rangeOne.max), rangeOne.min);
 			let expectedOutputTwo = arrayToRangeOutput(BASE_PATH, `${BASE_PATH}ById`,
@@ -131,7 +140,7 @@ describe("get core", function() {
 					.should.eventually.deep.equal(expectedOutputTwo);
 		});
 
-		it('returns a formatted error when unsuccessful', async function () {
+		it('returns a formatted error when unsuccessful (invalid id)', async function () {
 			let range = {
 				min: RANGE_MAX - RANGE_OVERFLOW,
 				max: RANGE_MAX + RANGE_OVERFLOW
@@ -141,7 +150,8 @@ describe("get core", function() {
 										 createGetRangesRoute(BASE_PATH, badRangePromise) ];
 			testServer.restart(routes);
 
-			let expectedOutput = generateRangeErrors(range.min, range.max, BASE_PATH,
+			// Generate expected output
+			let expectedOutput = generateRangeError(range.min, range.max, BASE_PATH,
 				TYPE_ERROR_ID_UNDEFINED);
 
 			// Test!!
@@ -157,9 +167,10 @@ describe("get core", function() {
 										 createGetRangesRoute(BASE_PATH, badRangePromise) ];
 			testServer.restart(routes);
 
+			// Generate expected outputs
 			let expectedOutputOne = arrayToRangeOutput(BASE_PATH, `${BASE_PATH}ById`,
 				await rangePromise(rangeOne.min, rangeOne.max), rangeOne.min);
-			let expectedOutputTwo = generateRangeErrors(rangeTwo.min, rangeTwo.max, BASE_PATH,
+			let expectedOutputTwo = generateRangeError(rangeTwo.min, rangeTwo.max, BASE_PATH,
 				TYPE_ERROR_ID_UNDEFINED);
 
 			// Test!!
@@ -183,5 +194,83 @@ describe("get core", function() {
 			return expect(route['route']).to.equal(ID_PATH) &&
 						 expect(route['get']).to.be.a('Function');
     });
+
+		_.times(ID_REPEAT, function () {
+			let id = generateIndex(RANGE_MIN, RANGE_MAX);
+			let keys = generateKeys(ACCEPTED_KEYS);
+			it(`returns data for ${keys.join(', ')} on id ${id}`, async function () {
+				// Create falcor route:
+	      let routes = [ createGetByIdRoute(BASE_PATH, ACCEPTED_KEYS, idPromise) ];
+				testServer.restart(routes);
+
+				// Generate expected output
+				let expectedOutput = objectToIdOutput(`${BASE_PATH}ById`,
+					await idPromise(id), keys);
+
+				// Test!!
+				return model.get(`${BASE_PATH}ById[${id}]["${keys.join('", "')}"]`)
+					.should.eventually.deep.equal(expectedOutput);
+			});
+		});
+
+		it('returns data when multiple ids are batched', async function () {
+			let idOne = generateIndex(RANGE_MIN, RANGE_MAX);
+			let keysOne = generateKeys(ACCEPTED_KEYS);
+			let idTwo = generateIndex(RANGE_MIN, RANGE_MAX);
+			let keysTwo = generateKeys(ACCEPTED_KEYS);
+			// Create falcor route:
+			let routes = [ createGetByIdRoute(BASE_PATH, ACCEPTED_KEYS, idPromise) ];
+			testServer.restart(routes);
+
+			// Generate expected outputs
+			let expectedOutputOne = objectToIdOutput(`${BASE_PATH}ById`,
+				await idPromise(idOne), keysOne);
+			let expectedOutputTwo = objectToIdOutput(`${BASE_PATH}ById`,
+				await idPromise(idTwo), keysTwo);
+
+			// Test!!
+			return batchModel.get(`${BASE_PATH}ById[${idOne}]["${keysOne.join('", "')}"]`)
+					.should.eventually.deep.equal(expectedOutputOne) &&
+				batchModel.get(`${BASE_PATH}ById[${idTwo}]["${keysTwo.join('", "')}"]`)
+					.should.eventually.deep.equal(expectedOutputTwo);
+		});
+
+		it('returns a formatted error when unsuccessful (invalid id)', async function () {
+			let id = RANGE_MAX + RANGE_OVERFLOW;
+			let keys = generateKeys(ACCEPTED_KEYS);
+			// Create falcor route:
+			let routes = [ createGetByIdRoute(BASE_PATH, ACCEPTED_KEYS, idPromise) ];
+			testServer.restart(routes);
+
+			// Generate expected output
+			let expectedOutput = generateIdError(id, keys, `${BASE_PATH}ById`,
+				TYPE_ERROR_ID_UNDEFINED);
+
+			// Test!!
+			return model.get(`${BASE_PATH}ById[${id}]["${keys.join('", "')}"]`)
+				.should.eventually.be.rejectedWith(expectedOutput);
+		});
+
+		it('returns a formatted error and data when multiple ranges are batched', async function () {
+			let idOne = RANGE_MAX + RANGE_OVERFLOW;
+			let keysOne = generateKeys(ACCEPTED_KEYS);
+			let idTwo = generateIndex(RANGE_MIN, RANGE_MAX);
+			let keysTwo = generateKeys(ACCEPTED_KEYS);
+			// Create falcor route:
+			let routes = [ createGetByIdRoute(BASE_PATH, ACCEPTED_KEYS, idPromise) ];
+			testServer.restart(routes);
+
+			// Generate expected outputs
+			let expectedOutputOne = generateIdError(idOne, keysOne, `${BASE_PATH}ById`,
+				TYPE_ERROR_ID_UNDEFINED);
+			let expectedOutputTwo = objectToIdOutput(`${BASE_PATH}ById`,
+				await idPromise(idTwo), keysTwo);
+
+			// Test!!
+			return batchModel.get(`${BASE_PATH}ById[${idOne}]["${keysOne.join('", "')}"]`)
+					.should.eventually.be.rejectedWith(expectedOutputOne) &&
+				batchModel.get(`${BASE_PATH}ById[${idTwo}]["${keysTwo.join('", "')}"]`)
+					.should.eventually.deep.equal(expectedOutputTwo);
+		});
   });
 });
--- test/utils/helpers.js
+++ test/utils/helpers.js
@@ -1,7 +1,16 @@
 import * as _ from 'lodash';
 
+import serialize from '../../src/serialization';
+
 export function generateIndex(min, max) {
 	return _.random(min, max);
+}
+
+export function generateKeys(acceptedKeys) {
+	let n = _.random(0, acceptedKeys.length - 1);
+	let k = _.shuffle(acceptedKeys);
+	return _.drop(k, n);
+
 }
 
 export function generateRange(min, max) {
@@ -10,14 +19,25 @@ export function generateRange(min, max) {
 	return { min: floor, max: floor + dif };
 }
 
-export function generateRangeErrors(min, max, path, error) {
-	let response = []
+export function generateRangeError(min, max, path, error) {
+	let response = [];
 	_.times(max - min, function(i) {
 		response.push({
 			path: [ path, min + i ],
 			value: {
 				message: error
 			}
+		});
+	});
+	return response;
+}
+
+export function generateIdError(id, keys, path, error) {
+	let response = [];
+	_.forEach(keys, function (k) {
+		response.push({
+			path: [ path, id, k ],
+			value: { message: error }
 		});
 	});
 	return response;
@@ -30,5 +50,18 @@ export function arrayToRangeOutput(path, modelPath, array, min) {
 		response.json[path][v.id] = [ modelPath, v.id ];
 	});
 	response.json[path]['$__path'] = [ path ];
+	return response;
+}
+
+export function objectToIdOutput(path, object, keys) {
+	let response = { json: { [path]: {
+		[object.id]: {
+			'$__path': [ path, object.id ]
+		},
+		['$__path']: [ path ]
+	} } };
+	_.forEach(keys, function(k) {
+		response.json[path][object.id][k] = serialize(object[k]);
+	});
 	return response;
 }
